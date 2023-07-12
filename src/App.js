4b201b5059898960e5ad09f3965ea655a27f563d
Anastasia <nastusya89pl@gmail.com>
2023-07-12 23:52:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+    };
+
+  const handleDecrement = () => {
+   if (counter > 1) setCounter(counter - 1); 
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+<>
+<h2>Counter: {counter} </h2>
+<button onClick={handleIncrement}>+</button>
+<button onClick={handleDecrement}>-</button>
+</>
+</div>
+  )}
 
-export default App;
+  export default App;
